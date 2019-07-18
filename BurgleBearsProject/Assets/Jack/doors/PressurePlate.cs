f61ab64d6c30445b97c10a29dbc0562ff75f5d29
JackChateauLoney <32193286+JackChateauLoney@users.noreply.github.com>
2019-07-18 14:30:49 +0100

    door with single button

---
+++ BurgleBearsProject/Assets/Jack/doors/PressurePlate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    public Animator anim;
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        anim.SetBool("open", true);
+        anim.SetBool("close", false);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        anim.SetBool("open", false);
+        anim.SetBool("close", true);
+    }
+}
